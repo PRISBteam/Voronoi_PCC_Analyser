7cbb867229b25fdd30858d27a8b2e6f79cd779ee
oubush <oubush@users.noreply.github.com>
2022-06-01 20:26:15 +0100

    create matutils.py

---
+++ matgen/matutils.py
@@ -0,0 +1,30 @@
+"""
+"""
+
+import numpy as np
+from scipy import sparse
+import networkx as nx
+
+def load_matrix_coo(f: open, matrix_shape=None) -> sparse.coo_matrix:
+    """
+    """
+    A_sparse = np.loadtxt(f, dtype='int')
+    I = np.array([row[0] for row in A_sparse]) - 1
+    J = np.array([row[1] for row in A_sparse]) - 1
+    V = np.array([row[2] for row in A_sparse])
+
+    A_coo = sparse.coo_matrix((V,(I,J)), shape=matrix_shape)
+
+    return A_coo
+
+
+def get_graph_from_A(f: open) -> nx.Graph:
+    """
+    A - adjacency matrix
+    """
+    A_sparse = np.loadtxt(f, dtype='int')
+    IJ = [(row[0], row[1]) for row in A_sparse]
+    G = nx.Graph()
+    G.add_edges_from(IJ)
+
+    return G
