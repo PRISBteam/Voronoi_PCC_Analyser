6684e95f68e90f6a6b3c6cd8cea2111ab7284f85
oubush <oubush@users.noreply.github.com>
2022-06-22 12:11:38 +0100

    update file save

--- matgen/matutils.py
+++ matgen/matutils.py
@@ -6,12 +6,17 @@ https://docs.scipy.org/doc/scipy/reference/sparse.csgraph.html#module-scipy.spar
 https://docs.scipy.org/doc/scipy/reference/sparse.linalg.html#module-scipy.sparse.linalg
 """
 
+import os
+from typing import Dict, List, Tuple, Union
 import numpy as np
 from scipy import sparse
 import networkx as nx
 
+from matgen.core import CellComplex
+
 def load_matrix_coo(f: open, matrix_shape=None) -> sparse.coo_matrix:
     """
+    TODO: change behavior ? (delete "- 1")?
     """
     A_sparse = np.loadtxt(f, dtype='int')
     I = np.array([row[0] for row in A_sparse]) - 1
@@ -23,7 +28,7 @@ def load_matrix_coo(f: open, matrix_shape=None) -> sparse.coo_matrix:
     return A_coo
 
 
-def get_graph_from_A(f: open) -> nx.Graph:
+def get_graph_from_A_file(f: open) -> nx.Graph:
     """
     A - adjacency matrix
     """
@@ -33,3 +38,76 @@ def get_graph_from_A(f: open) -> nx.Graph:
     G.add_edges_from(IJ)
 
     return G
+
+def get_graph_from_A_file(f: open) -> nx.Graph:
+    """
+    A - adjacency matrix
+    """
+    A_sparse = np.loadtxt(f, dtype='int')
+    IJ = [(row[0], row[1]) for row in A_sparse]
+    G = nx.Graph()
+    G.add_edges_from(IJ)
+
+    return G
+
+
+def _get_IJV_from_neighbors(_cells: Dict) -> Tuple[List]:
+    """
+    index of an element is element_id - 1
+    """
+
+    I = []
+    J = []
+    V = []
+    for cell_id, cell in _cells.items():
+        for n_id in cell.neighbor_ids:
+            I.append(cell_id - 1)
+            J.append(n_id - 1)
+            V.append(1)
+    
+    return (I, J, V)
+
+
+def get_G_from_cells(_cells: Dict):
+    """
+    """
+    I, J, _ = _get_IJV_from_neighbors(_cells)
+    IJ = [(i + 1, j + 1) for i, j in zip(I, J)]
+    G = nx.Graph()
+    G.add_edges_from(IJ)
+    
+    return G
+
+
+def get_A_from_cells(_cells: Dict):
+    """
+    """
+    I, J, V = _get_IJV_from_neighbors(_cells)
+    A_coo = sparse.coo_matrix((V,(I,J)))
+    return A_coo
+
+
+def save_A(
+        c: CellComplex,
+        work_dir: str = '.'):
+    """
+    """
+    # Save A0.txt
+    filename = os.path.join(work_dir, 'A0.txt')
+    I, J, V = _get_IJV_from_neighbors(c._vertices)
+    np.savetxt(filename, [*zip(I, J, V)], fmt='%d')
+
+    # Save A1.txt
+    filename = os.path.join(work_dir, 'A1.txt')
+    I, J, V = _get_IJV_from_neighbors(c._edges)
+    np.savetxt(filename, [*zip(I, J, V)], fmt='%d')
+
+    # Save A2.txt
+    filename = os.path.join(work_dir, 'A2.txt')
+    I, J, V = _get_IJV_from_neighbors(c._faces)
+    np.savetxt(filename, [*zip(I, J, V)], fmt='%d')
+
+    # Save A3.txt
+    filename = os.path.join(work_dir, 'A3.txt')
+    I, J, V = _get_IJV_from_neighbors(c._polyhedra)
+    np.savetxt(filename, [*zip(I, J, V)], fmt='%d')
