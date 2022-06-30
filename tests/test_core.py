375b7cbd66b84c369fd6d6feb128d463e22c7892
oubush <oubush@users.noreply.github.com>
2022-06-30 11:30:44 +0100

    update Edge tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -31,6 +31,11 @@ def test_Vertex():
     assert v.coord2D == pytest.approx((0.34, 0.58))
     assert v.__str__() == "Vertex(id=42)"
 
+
+def test_Vertex_add_neighbors():
+    """
+    """
+    v = core.Vertex(id=42, x=0.34, y=0.58, z=0.89)
     v.add_neighbor(21)
     v.add_neighbor(34)
     v.add_neighbors([1, 2])
@@ -38,9 +43,12 @@ def test_Vertex():
     v.add_neighbor(2)
     assert set(v.neighbor_ids) == {1, 2, 3, 5, 21, 34}
     assert len(v.neighbor_ids) == 6
-    # for v_id in [1, 2, 3, 5, 21, 34]:
-    #     assert v_id in v.neighbor_ids
-    
+
+
+def test_Vertex_add_incident():
+    """
+    """
+    v = core.Vertex(id=42, x=0.34, y=0.58, z=0.89)
     v.add_incident_edge(21)
     v.add_incident_edge(34)
     v.add_incident_edges([1, 2])
@@ -54,6 +62,11 @@ def test_Vertex():
     for e_id in [1, 2, 3, 5, 21, 34]:
         assert e_id in v.incident_cells
 
+
+def test_Vertex_set():
+    """
+    """
+    v = core.Vertex(id=42, x=0.34, y=0.58, z=0.89)
     assert not v.is_external
     v.set_external(True)
     assert v.is_external
@@ -65,6 +78,10 @@ def test_Vertex():
     v.set_junction_type('J1')
     assert v.junction_type == 'J1'
 
+
+def test_Vertex_plot():
+    """
+    """
     points = [
         (0.0, 0.0),
         (0.0, 0.5),
@@ -108,6 +125,10 @@ def test_Vertex():
     ax.legend(loc='best')
     plt.savefig('tests/figures/vertices-2D-1.png')
 
+
+def test_Vertex_from_tess_file():
+    """
+    """
     filename = 'tests/test_data/n8-id1.tess'
     _vertices = core.Vertex.from_tess_file(filename)
     assert len(_vertices.keys()) == 41
@@ -134,6 +155,11 @@ def test_Edge():
     assert e.v_ids == [5, 1]
     assert e.__str__() == "Edge(id=42)"
 
+
+def test_Edge_add_neighbors():
+    """
+    """
+    e = core.Edge(id=42, v_ids=[5, 1])
     e.add_neighbor(21)
     e.add_neighbor(34)
     e.add_neighbors([1, 2])
@@ -142,7 +168,11 @@ def test_Edge():
     assert len(e.neighbor_ids) == 6
     for e_id in [1, 2, 3, 5, 21, 34]:
         assert e_id in e.neighbor_ids
-    
+
+def test_Edge_add_incident():
+    """
+    """
+    e = core.Edge(id=42, v_ids=[5, 1])
     e.add_incident_face(21)
     e.add_incident_face(34)
     e.add_incident_faces([1, 2])
@@ -156,38 +186,90 @@ def test_Edge():
     for f_id in [1, 2, 3, 5, 21, 34]:
         assert f_id in e.incident_cells
 
+def test_Edge_set():
+    """
+    """
+    e = core.Edge(id=42, v_ids=[5, 1])
     e.set_length(93.45)
     assert e.len == pytest.approx(93.45)
-
-    assert not e.is_external
-    assert not e.is_special
-    e.set_external()
-    assert e.is_external
-    assert not e.is_special
-    assert e.theta == pytest.approx(-1)
-    e.set_theta(10, threshold=15)
-    assert not e.is_external
-    assert not e.is_special
-    assert e.theta == pytest.approx(10)
-    e.set_theta(60, threshold=15)
-    assert not e.is_external
-    assert e.is_special
-    assert e.theta == pytest.approx(60)
-    e.set_theta(-1, threshold=15)
-    assert e.is_external
-    assert not e.is_special
-    e.set_special()
-    assert not e.is_external
-    assert e.is_special
-    assert e.theta is None
 
     e.set_junction_type('E')
     assert e.junction_type == 'E'
     e.set_junction_type('J1')
     assert e.junction_type == 'J1'
 
+def test_Edge_set_special():
+    """
+    """
+    e = core.Edge(id=42, v_ids=[5, 1])
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta is None
+    e.set_external()
+    assert e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(-1)
+    with pytest.raises(ValueError, match="External doesn't have theta"):
+        e.set_theta(10)
+    assert e.is_external
+    e.set_external(False)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta is None
+    e.set_special()
+    assert not e.is_external
+    assert e.is_special    
+    assert e.theta is None
+    e.set_special(False)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta is None
+
+    e.set_theta(60)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(60)
+    e.set_theta(58, 15, 58)
+    assert not e.is_external
+    assert e.is_special
+    assert e.theta == pytest.approx(58)
+    e.set_theta(12, 15, 58)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(12)
+    e.set_theta(60, 15, 58)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(60)    
+    e.set_theta(30, 15)
+    assert not e.is_external
+    assert e.is_special
+    assert e.theta == pytest.approx(30)
+    e.set_theta(30, 15)
+    assert not e.is_external
+    assert e.is_special
+    assert e.theta == pytest.approx(30)    
+    e.set_theta(30, upper_thrd=15)
+    assert not e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(30) 
+    e.set_special()
+    assert not e.is_external
+    assert e.is_special
+    e.set_theta(-1, 15, 62)
+    assert e.is_external
+    assert not e.is_special
+    with pytest.raises(ValueError, match="External cannot be set special"):
+        e.set_special()
+    assert e.is_external
+    assert not e.is_special
+    assert e.theta == pytest.approx(-1)
+
+
+def test_Edge_from_tess_file():
+    """
+    """
     filename = 'tests/test_data/n8-id1.tess'
-    
     _edges = core.Edge.from_tess_file(filename)
     assert len(_edges.keys()) == 78
     assert _edges[1].id == 1
@@ -227,7 +309,7 @@ def test_Edge():
 
     filename = 'tests/test_data/n8-id1-2D-reg.tess'
     _edges = core.Edge.from_tess_file(
-        filename, measure=True, theta=True, theta_threshold=15)
+        filename, measure=True, theta=True, lower_thrd=15)
     assert len(_edges.keys()) == 24
     assert _edges[1].id == 1
     assert _edges[11].__str__() == 'Edge(id=11)'
@@ -249,7 +331,7 @@ def test_Edge():
     assert len([e.id for e in _edges.values() if e.is_external]) == 10
 
     _edges = core.Edge.from_tess_file(
-        filename, measure=True, theta=True, theta_threshold=70)
+        filename, measure=True, theta=True, lower_thrd=70, upper_thrd=100)
     assert _edges[4].theta == pytest.approx(67.795707673672)
     assert not _edges[4].is_external
     assert not _edges[4].is_special
@@ -257,7 +339,9 @@ def test_Edge():
     assert not _edges[5].is_external
     assert _edges[5].is_special
     assert _edges[5].len == pytest.approx(0.217043601663)
-
+    assert _edges[7].theta == pytest.approx(135.438499725622)
+    assert not _edges[7].is_external
+    assert not _edges[7].is_special
 
 
 def test_CellComplex():
