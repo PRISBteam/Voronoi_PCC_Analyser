0e0e8b3530ab6eaa13db22e8fcd5c3e132215d1f
oubush <oubush@users.noreply.github.com>
2022-08-03 11:56:54 +0100

    update sim2D

--- sim2D.py
+++ sim2D.py
@@ -36,19 +36,28 @@ for m in tqdm(range(10)):
     e_spec = c.get_special_ids()
     
     e_sel = list(set(e_int) - set(e_spec))
-    e_id_sampled = random.sample(e_sel, 1)[0]
+    if e_sel:
+        e_id_sampled = random.sample(e_sel, 1)[0]
+    else:
+        print('All GB are special')
+        break
+    
+    G_frac = round(m / n, 3)
+    GB_frac = round(len(e_spec) / len(e_int), 3)
+
     x, y = simulation.get_new_seed_2D(c, e_id_sampled)
     ax = c.plot_edges(e_sel, color='b')
     if e_spec:
         ax = c.plot_edges(e_spec, color='g', ax=ax)
     ax = c.plot_edges(e_ext, color='k', ax=ax)
     ax.scatter(x, y, color='r')
+    ax.set_title(f'New G: {m} G frac: {G_frac} GB frac: {GB_frac}')
     plt.savefig(output_file.rstrip('.tess') + '.png')
     plt.close()
     with open(seeds_filename, 'a') as file:
         file.write('%.12f %.12f\n' % (x, y))
     print('Total grains:', n, 'New grains:', m)
-    print('New grain fraction:', m / n)
-    print('Special GB fraction:', len(e_spec) / len(e_int))
+    print('New grain fraction:', G_frac)
+    print('Special GB fraction:', GB_frac)
 
 print('\nTotal time:', time.time() - st, 's')
