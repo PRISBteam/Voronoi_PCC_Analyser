3f1af3384fb5abbdcbce01eca41938aafc262ee7
oubush <oleg.u.bushuev@gmail.com>
2022-09-21 17:16:18 +0100

    add files parsing

--- characterise.py
+++ characterise.py
@@ -5,6 +5,9 @@ import argparse
 import os
 import time
 import glob
+import logging
+
+from matgen import base
 
 def main() -> None:
     """
@@ -28,9 +31,29 @@ def main() -> None:
     args = parser.parse_args()
     filenames = glob.glob(args.dir + '/*')
 
-    for filename in filenames:
-        pass
+    TJsets = {}
+    complex_id = 0
 
+    for filename in filenames:
+        file = open(filename, 'r')
+        is_tess = 'tess' in file.read()
+        if is_tess:
+            pass
+        else:
+            try:
+                file.seek(0)
+                for line in file:
+                    complex_id += 1
+                    row = [*map(float, line.split())]
+                    if len(row) != 5:
+                        raise ValueError()
+                    p = row[0]
+                    j_tuple = tuple(row[1:])
+                    TJsets[complex_id] = base.TripleJunctionSet(p, j_tuple)
+            except ValueError:
+                logging.exception(f'Wrong file: {filename}')
+            except:
+                logging.exception('Some error')
 
     print('Time elapsed:', round(time.time() - start, 2), 's')
 
