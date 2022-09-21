a93cc32558ebf3a4750cb4d21ad828028dc372fa
oubush <oleg.u.bushuev@gmail.com>
2022-09-21 16:35:41 +0100

    create characterise.py

---
+++ characterise.py
@@ -0,0 +1,37 @@
+"""
+"""
+
+import argparse
+import os
+import time
+import glob
+
+def main() -> None:
+    """
+    """
+    start = time.time()
+    
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument(
+    #     "--file",
+    #     default='complex.tess',
+    #     help="Filepath to .tess file with a tesselation (Neper output)"
+    # )
+
+    parser.add_argument(
+        "--dir",
+        default='.',
+        help="Path to folder which contain complexes to characterise"
+    )
+
+    args = parser.parse_args()
+
+    print('Complexes: ', glob.glob(args.dir + '/*.tess'))
+    print('TXT: ', glob.glob(args.dir + '/*.txt'))
+
+    print('Time elapsed:', round(time.time() - start, 2), 's')
+
+
+if __name__ == "__main__":
+    main()
