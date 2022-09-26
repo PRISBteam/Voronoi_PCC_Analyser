ff8bba897d5121c93cb3331a843931b93e78ca82
oubush <oleg.u.bushuev@gmail.com>
2022-09-26 15:52:31 +0100

    add TripleJunction, GrainBoundary

--- matgen/base.py
+++ matgen/base.py
@@ -1,6 +1,7 @@
 """Base classes for complex analysis.
 """
-from typing import Iterable, List
+import io
+from typing import Dict, Iterable, List, Tuple
 from matgen import matutils
 # import numpy as np
 
@@ -14,6 +15,7 @@ class Cell():
 
         self.id = id
         self.n_ids = [] # neighbour ids
+        self.is_external = False
 
     def __str__(self):
         """
@@ -38,6 +40,17 @@ class Cell():
         """
         self.n_ids += n_ids
         self.n_ids = list(set(self.n_ids))
+
+    def set_external(self, is_external: bool = True):
+        """
+        """
+        self.is_external = is_external
+
+    def set_measure(self, measure: float):
+        """
+        """
+        self.measure = measure
+
 
 
 class CellLowerDim(Cell):
@@ -74,7 +87,52 @@ class CellLowerDim(Cell):
     #     self.incident_ids += incident_ids
     #     self.incident_ids = list(set(self.incident_ids))
 
+    def get_degree(self) -> int:
+        """
+        """
+        return len(self.signed_incident_ids)
 
+
+class TripleJunction(CellLowerDim):
+    """
+    """
+    def __init__(self, id: int):
+        super().__init__(id)
+        self.junction_type = None
+
+    def __str__(self):
+        """
+        """
+        cell_str = "TripleJunction(id=%d)" % self.id
+        
+        return cell_str
+
+    def set_junction_type(self, junction_type: int):
+        """
+        junction_type equals number of special incident cells
+        """
+        self.junction_type = junction_type
+
+
+class GrainBoundary(CellLowerDim):
+    """
+    """
+    def __init__(self, id: int):
+        super().__init__(id)
+        self.is_special = False
+        self.theta = None
+
+    def __str__(self):
+        """
+        """
+        cell_str = "GrainBoundary(id=%d)" % self.id
+        
+        return cell_str
+
+    def set_special():
+        pass
+
+    
 class Vertex(CellLowerDim):
     """
     """
@@ -84,6 +142,25 @@ class Vertex(CellLowerDim):
         self.y = y
         self.z = z
 
+    @classmethod
+    def from_tess_file(cls, file: io.TextIOBase) -> Dict:
+        """
+        Note that file must be oper prior to calling this method
+        Be careful with reading the same file with different methods
+        """      
+        _vertices = {}
+        for line in file:
+            if '**vertex' in line:
+                n = int(file.readline().rstrip('\n'))
+                for i in range(n):
+                    row = file.readline().split()
+                    v_id = int(row[0])
+                    x = float(row[1])
+                    y = float(row[2])
+                    z = float(row[3])
+                    _vertices[v_id] = cls(v_id, x, y, z)
+                return _vertices
+
     def __str__(self) -> str:
         """
         """
@@ -91,6 +168,34 @@ class Vertex(CellLowerDim):
             self.id, self.x, self.y, self.z
         )
         return v_str
+
+    @property
+    def coord(self) -> Tuple[float]:
+        """
+        """
+        return (self.x, self.y, self.z)
+
+
+class Vertex2D(Vertex, TripleJunction):
+    """
+    """
+    def __init__(self, id: int, x: float, y: float, z: float = 0):
+        super().__init__(id, x, y, z)
+        self.junction_type = None
+
+    def __str__(self) -> str:
+        """
+        """
+        v_str = "Vertex2D(id=%d, x=%.3f, y=%.3f)" % (
+            self.id, self.x, self.y
+        )
+        return v_str
+
+    @property
+    def coord(self) -> Tuple[float]:
+        """
+        """
+        return (self.x, self.y)
 
 
 class Edge(CellLowerDim):
@@ -104,9 +209,47 @@ class Edge(CellLowerDim):
         """
         """
         e_str = "Edge(id=%d)" % self.id
+        return e_str
+
+    @property
+    def len(self) -> float:
+        """
+        """
+        try:
+            return self.measure
+        except AttributeError:
+            return None 
+
+
+class Edge3D(Edge, TripleJunction):
+    """
+    """
+    def __init__(self, id: int, v_ids: Iterable):
+        super().__init__(id, v_ids)
+        self.junction_type = None
+
+    def __str__(self):
+        """
+        """
+        e_str = "Edge3D(id=%d)" % self.id
         
         return e_str
-    
+
+
+class Edge2D(Edge, GrainBoundary):
+    """
+    """
+    def __init__(self, id: int, v_ids: Iterable):
+        super().__init__(id, v_ids)
+        self.is_special = False
+
+    def __str__(self):
+        """
+        """
+        e_str = "Edge2D(id=%d)" % self.id
+        
+        return e_str
+
 
 class Face(CellLowerDim):
     """
